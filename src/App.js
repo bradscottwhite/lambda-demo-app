51782b5f03536ffe8cf48cdc36345ccf8fd4599d
Brad White <bradscottwhite@gmail.com>
2023-01-13 05:26:41 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * ...
+ */
 
-function App() {
+import { useState } from 'react';
+
+import axios from 'axios';
+
+const url = 'https://pbaesib7i1.execute-api.us-east-1.amazonaws.com/prod/hello'; // ***URL_FROM_LAST_TUTORIAL***
+
+const App = () => {
+  const [ name, setName ] = useState('');
+  const [ greeting, setGreeting ] = useState('');
+
+  const fetchGreeting = () => {
+    axios.post(url, { name })
+      .then(res => {
+        const { data } = res;
+        setGreeting(data.msg);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bg-red-500">
+      <header>
+        <input
+          value={name}
+          placeholder='Enter name here'
+          onChange={e => setName(e.currentTarget.value)}
+        />
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <h2>{greeting}</h2>
       </header>
     </div>
   );
-}
+};
 
 export default App;
